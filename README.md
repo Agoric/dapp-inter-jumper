1f7ede712f88728cfa38f6768536d1a5c0b710fd
Samuel Siegart <samsiegart@gmail.com>
2023-09-17 12:08:10 -0700

    (feat): allow for banner controlled by network-config (#14)
    Co-authored-by: Ivan Leichtling <ivanlei@gmail.com>

--- README.md
+++ README.md
@@ -11,8 +11,12 @@ site as voted on by the Inter Protocol Econ Committee.
 
 To use a different network config add `?network=<NETWORK NAME>` to the URL.
 Then it will use `https://<NETWORK NAME>.agoric.net/network-config`.
+
 For local development, use `?network=local`, then it will use
 `https://wallet.agoric.app/wallet/network-config`
+
+When `network=local` you may also set `?banner=blahblah` to test banners.
+`?network=local&banner=are%20you%20seeing%20this%20banner`
 
 # Deployment
 
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
 import { vstorageQuery } from './vstorage-query';
 
-const fetchRPCAddr = async (netconfigURL: string) => {
-  const response = await fetch(netconfigURL, {
-    headers: { accept: 'application/json' },
-  });
-  const { rpcAddrs } = await response.json();
-
-  return rpcAddrs[Math.floor(Math.random() * rpcAddrs.length)];
-};
-
 const agoricNetName =
   new URLSearchParams(window.location.search).get('network') ?? 'main';
 
@@ -23,7 +14,6 @@ const networkConfigUrl = (() => {
 const redirectParams = (() => {
   if (agoricNetName !== 'main') {
     const redirectParams = new URLSearchParams();
-    redirectParams.append('wallet', 'main');
     redirectParams.append('network', agoricNetName);
     return '?' + redirectParams.toString();
   } else {
@@ -31,38 +21,112 @@ const redirectParams = (() => {
   }
 })();
 
-const setMessage = (message: string) => {
-  document?.getElementById('msg')?.replaceChildren(message);
+const bannerParams = (() => {
+  if (agoricNetName === 'local') {
+    return new URLSearchParams(window.location.search).get('banner') ?? '';
+  } else {
+    return '';
+  }
+})();
+
+const fetchNetworkConfig = async (netconfigURL: string) => {
+  const response = await fetch(netconfigURL, {
+    headers: { accept: 'application/json' },
+  });
+  const { rpcAddrs, dappInterJumperBanner } = await response.json();
+
+  return {
+    rpc: rpcAddrs[Math.floor(Math.random() * rpcAddrs.length)],
+    banner: bannerParams || dappInterJumperBanner,
+  };
+};
+
+const hideMessage = () => {
+  const msg = document?.getElementById('msg')!;
+  msg.classList.add('hidden');
+  msg.classList.remove('flex');
+};
+
+const unHide = (element: HTMLElement) => element.classList.remove('hidden');
+
+const setBanner = (message: string) => {
+  const msg = document?.getElementById('msg')!;
+  msg.replaceChildren(message);
+  unHide(msg);
+  msg.classList.remove('animate-pulse');
+
+  const noticeHeader = document?.getElementById('notice-header')!;
+  unHide(noticeHeader);
+
   console.info(message);
 };
 
-const tryRedirect = async () => {
-  const rpc = await fetchRPCAddr(networkConfigUrl);
-  let referencedUI;
+const enableProceedButton = (redirectUrl: URL) => {
+  const proceedButtonContainer = document?.getElementById(
+    'proceed-button-container'
+  )!;
+  unHide(proceedButtonContainer);
 
+  const proceedButton = document?.getElementById('proceed-button')!;
+  proceedButton.onclick = () => location.replace(redirectUrl);
+};
+
+const showError = (msg: string) => {
+  const notFoundMessage = document?.getElementById('not-found')!;
+  unHide(notFoundMessage);
+  notFoundMessage.replaceChildren(msg);
+};
+
+const getRedirectUrl = async (rpc: string) => {
+  const data = await vstorageQuery(rpc, 'published.vaultFactory.governance');
+  const { values } = JSON.parse(data.value);
+  const latestValue = values[values.length - 1];
+  const value = JSON.parse(latestValue);
+  const body = JSON.parse(value.body.substring(1));
+  // TODO: remove backwards-compatible "EndorsedUI" after contract change.
+  const referencedUI = body.current.ReferencedUI || body.current.EndorsedUI;
+  const href = `https://${referencedUI.value}.ipfs.cf-ipfs.com/${redirectParams}`;
+  return new URL(href);
+};
+
+const tryRedirect = async () => {
+  let networkConfig;
   try {
-    const data = await vstorageQuery(rpc, 'published.vaultFactory.governance');
-    const { values } = JSON.parse(data.value);
-    const latestValue = values[values.length - 1];
-    const value = JSON.parse(latestValue);
-    const body = JSON.parse(value.body.substring(1));
-    // TODO: remove backwards-compatible "EndorsedUI" after contract change.
-    referencedUI = body.current.ReferencedUI || body.current.EndorsedUI;
+    networkConfig = await fetchNetworkConfig(networkConfigUrl);
   } catch {
-    setMessage('Not found.');
+    showError(`Network config not found for ${networkConfigUrl}`);
+    hideMessage();
     return;
   }
 
-  if (!referencedUI) {
-    setMessage('Not found.');
-  } else {
-    try {
-      const href = `https://${referencedUI.value}.ipfs.cf-ipfs.com/${redirectParams}`;
-      const redirectUrl = new URL(href);
+  // Satisfy typescript
+  if (!networkConfig) {
+    throw new Error('Unexpected error, network config undefined');
+  }
+
+  const rpc = networkConfig.rpc;
+  const banner = networkConfig.banner;
+  const timeoutDurationMS = 10_000;
+  let redirectUrl: URL | undefined = undefined;
+
+  try {
+    redirectUrl = await (Promise.race([
+      getRedirectUrl(rpc),
+      new Promise((_, rej) => setTimeout(rej, timeoutDurationMS)),
+    ]) as Promise<URL>);
+  } catch {
+    showError('No endorsed UI found.');
+  }
+
+  if (banner) {
+    setBanner(banner);
+  }
+
+  if (redirectUrl) {
+    if (banner) {
+      enableProceedButton(redirectUrl);
+    } else {
       location.replace(redirectUrl);
-    } catch (e) {
-      console.error(e, referencedUI.value);
-      setMessage('Not found: ' + referencedUI.value);
     }
   }
 };
